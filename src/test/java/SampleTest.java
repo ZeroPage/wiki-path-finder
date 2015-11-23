95e87ddf8db01e733f29ef6447b24d2989d99223
YeongJun Cho <skywave@live.co.kr>
2015-11-23 17:39:14 +0900

    test(SampleTest): added sample test

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SampleTest {
+    @Test
+    public void sampleTest() throws Exception {
+        assertEquals(true, true);
+    }
+}
