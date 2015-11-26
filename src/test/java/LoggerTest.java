2ee65c8e72b16e49d4322cf49cfd2ee8b1843b97
Indigobit <cho_hol@outlook.com>
2015-11-26 12:06:15 +0900

    test(LoggerTest) added LoggerTest. #9

--- src/test/java/LoggerTest.java
+++ src/test/java/LoggerTest.java
@@ -1,4 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
 
 public class LoggerTest {
-// TODO: write down test code
+    Logger logger;
+
+    @Before
+    public void setUp() {
+        logger = Logger.getInstance();
+    }
+
+    @Test
+    public void testPrintLogger() throws Exception {
+        logger.addListener(new OutputStreamLogListener(System.out));
+        logger.error("Test error message");
+    }
+
+    // TODO: write down more test code
 }
