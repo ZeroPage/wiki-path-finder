18f6a214d45b8d5c4c468101dfb61f26a8548c5b
Indigobit <cho_hol@outlook.com>
2015-11-26 11:59:48 +0900

    fix(Logger) fixed loop error.

--- src/main/java/Logger.java
+++ src/main/java/Logger.java
@@ -28,32 +28,32 @@ public class Logger {
     }
 
     public void debug(String message) {
-        for(int i = 0 ; i <= listeners.size(); i++) {
+        for(int i = 0 ; i < listeners.size(); i++) {
            listeners.get(i).debug(message);
         }
 
     }
 
     public void info(String message) {
-        for(int i = 0 ; i <= listeners.size(); i++) {
+        for(int i = 0 ; i < listeners.size(); i++) {
             listeners.get(i).info(message);
         }
     }
 
     public void warn(String message) {
-        for(int i = 0 ; i <= listeners.size(); i++) {
+        for(int i = 0 ; i < listeners.size(); i++) {
             listeners.get(i).warn(message);
         }
     }
 
     public void error(String message) {
-        for(int i = 0 ; i <= listeners.size(); i++) {
+        for(int i = 0 ; i < listeners.size(); i++) {
             listeners.get(i).error(message);
         }
     }
 
     public void fatal(String message) {
-        for(int i = 0 ; i <= listeners.size(); i++) {
+        for(int i = 0 ; i < listeners.size(); i++) {
             listeners.get(i).fatal(message);
         }
     }
