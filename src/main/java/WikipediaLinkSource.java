b028cf411c8873a7d5cf8175f153b99fd9129792
YeongJun Cho <skywave@live.co.kr>
2015-11-27 21:17:26 +0900

    feat(WikipediaLinkSource): implemented WikipediaLinkSource

--- src/main/java/WikipediaLinkSource.java
+++ src/main/java/WikipediaLinkSource.java
@@ -1,14 +1,24 @@
+import wiki_api.PageNotFoundException;
 import wiki_api.WikipediaApi;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
 public class WikipediaLinkSource implements LinkSource {
-    public WikipediaLinkSource(WikipediaApi api) {
+    private WikipediaApi api;
 
+    public WikipediaLinkSource(WikipediaApi api) {
+        this.api = api;
     }
 
     @Override
-    public Set<String> getLinks(String from) {
-        return null;
+    public Set<String> getLinks(String from) throws IOException {
+        try {
+            return new HashSet<>(Arrays.asList(api.getLinks(from)));
+        } catch (PageNotFoundException e) {
+            return null;
+        }
     }
 }
