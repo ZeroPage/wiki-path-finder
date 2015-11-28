56ff032e979459efa94f1329d020d69175559097
archetype <archetype93@gmail.com>
2015-11-28 23:25:21 +0900

    Test for MemCacheStorage
    test using hashset.

---
+++ src/test/java/MemCacheStorageTest.java
@@ -0,0 +1,41 @@
+import org.hamcrest.CoreMatchers;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+public class MemCacheStorageTest {
+    MemCacheStorage cache;
+    Set<String> testSample;
+
+    @Before
+    public void setUp() throws Exception {
+        cache = new MemCacheStorage();
+        testSample = new HashSet<>();
+        testSample.add("dog");
+        testSample.add("cat");
+        testSample.add("koibito");
+        cache.setData("white",testSample);
+    }
+
+    @Test
+    public void testGetData() throws Exception {
+        Set<String> test = cache.getData("white");
+        assertThat(test, CoreMatchers.is(testSample));
+    }
+
+    @Test
+    public void testSetData() throws Exception {
+        HashSet<String> newValue = new HashSet<>();
+        newValue.add("chistmas");
+        newValue.add("wine");
+        newValue.add("chocolate");
+
+        cache.setData("white",newValue);
+
+        assertThat(cache.getData("white"), CoreMatchers.is(CoreMatchers.not(testSample)));
+    }
+}
