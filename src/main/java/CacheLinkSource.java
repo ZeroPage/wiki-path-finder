5fd258cbbcf1d3d38dfe39f8c36f25e13ae8c2d8
archetype <archetype93@gmail.com>
2015-11-29 00:02:43 +0900

    basic implementation.
    disscussion needed for the detail of the class.

--- src/main/java/CacheLinkSource.java
+++ src/main/java/CacheLinkSource.java
@@ -1,13 +1,14 @@
 import java.util.Set;
 
 public class CacheLinkSource implements LinkSourceDecorator {
+    CacheStorage cache;
 
     public CacheLinkSource(CacheStorage cacheStorage) {
-
+        cache = cacheStorage;
     }
 
     @Override
     public Set<String> getLinks(String from) {
-        return null;
+        return cache.getData(from);
     }
 }
