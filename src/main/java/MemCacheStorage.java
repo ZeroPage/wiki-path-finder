17896f5131355548118ee34739ed6dc11a01ddb1
archetype <archetype93@gmail.com>
2015-11-28 22:37:51 +0900

    Simple Implementation using hashtable
    since hashtable is basically synced container, we don't have to handle
    the issue.
    If there's any problem, plz contact me ASAP.

--- src/main/java/MemCacheStorage.java
+++ src/main/java/MemCacheStorage.java
@@ -1,14 +1,16 @@
+import java.util.Hashtable;
 import java.util.Set;
 
 public class MemCacheStorage implements CacheStorage {
+    private static Hashtable<String,Set<String>> cacheTable = new Hashtable<>(100,1);
 
     @Override
     public Set<String> getData(String key) {
-        return null;
+        return cacheTable.get(key);
     }
 
     @Override
     public void setData(String key, Set<String> data) {
-
+        cacheTable.put(key,data);
     }
 }
