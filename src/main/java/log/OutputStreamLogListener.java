1f740d010b41f0527feee1e7d5671d0fc2ae361c
Indigobit <cho_hol@outlook.com>
2015-12-01 14:25:55 +0900

    refactor(OutputStreamLogListener): changed variable default to private

--- src/main/java/log/OutputStreamLogListener.java
+++ src/main/java/log/OutputStreamLogListener.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 public class OutputStreamLogListener implements LogListener {
-    OutputStream outputStream;
+    private OutputStream outputStream;
 
     public OutputStreamLogListener(OutputStream outputStream) {
         this.outputStream = outputStream;
