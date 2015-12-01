3f96db92fe26d78001be613dd2049db456964b28
Indigobit <cho_hol@outlook.com>
2015-12-01 14:32:20 +0900

    feat(LogListener): added level enum

--- src/main/java/log/LogListener.java
+++ src/main/java/log/LogListener.java
@@ -1,6 +1,8 @@
 package log;
 
 public interface LogListener {
+    enum Level { FATAL, ERROR, WARN, INFO, DEBUG }
+
     void debug(String message);
     void info(String message);
     void warn(String message);
