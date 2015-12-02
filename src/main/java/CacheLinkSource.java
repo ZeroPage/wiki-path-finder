831ac3810de8a80c1a6b1fd8376425abcab03599
archetype <archetype93@gmail.com>
2015-12-02 16:17:08 +0900

    feat(CacheLinkSource): Proper implementation as Decorator Pattern

--- src/main/java/CacheLinkSource.java
+++ src/main/java/CacheLinkSource.java
@@ -2,13 +2,22 @@ import java.util.Set;
 
 public class CacheLinkSource implements LinkSourceDecorator {
     CacheStorage cache;
+    LinkSource innerSource;
 
-    public CacheLinkSource(CacheStorage cacheStorage) {
-        cache = cacheStorage;
+    public CacheLinkSource(LinkSource innerObject, CacheStorage cacheStorage) {
+        this.innerSource = innerObject;
+        this.cache = cacheStorage;
     }
 
     @Override
-    public Set<String> getLinks(String from) {
-        return cache.getData(from);
+    public Set<String> getLinks(String from) throws Exception {
+        if(this.cache.getData(from)==null) {
+            if(innerSource==null) {
+                return null;
+            } else {
+                return innerSource.getLinks(from);
+            }
+        }
+        return this.cache.getData(from);
     }
 }
