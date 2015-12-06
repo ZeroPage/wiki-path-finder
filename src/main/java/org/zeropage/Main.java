39a8152b57d0302cf0e52a11b2b59eb74cc3d137
archetype <archetype93@gmail.com>
2015-12-06 23:22:06 +0900

    feat(Main): implementation of functional method, and sample implementation
    .

--- src/main/java/org/zeropage/Main.java
+++ src/main/java/org/zeropage/Main.java
@@ -1,9 +1,38 @@
 package org.zeropage;
 
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // hello world for java8
-        Runnable r = () -> System.out.println("Hello, World!");
-        r.run();
+        PathFinder pFinder;
+        /* Implement the detailed interaction, and instantiate pFinder
+         * From here, t'is only the simple implementation of the main method. */
+        pFinder = new SimplePathFinder(new MockLinkSource());
+        run(pFinder);
+    }
+
+    private static void run(PathFinder pFinder) {
+        Iterator<String> pIterator;
+        String from, to;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("From: ");//4
+        from = input.next();
+        System.out.print("To: ");//3
+        to = input.next();
+
+        try {
+            pIterator = pFinder.getPath(from, to).getPathIterator();
+            while(pIterator.hasNext()) {
+                System.out.print(pIterator.next());
+                if(pIterator.hasNext()) {
+                    System.out.print("->");
+                }
+            }
+        } catch(Exception e) {
+            /* What can I do here?
+             * Help me, skywave! */
+        }
     }
 }
