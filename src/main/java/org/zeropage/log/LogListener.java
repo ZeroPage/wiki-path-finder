36a8ecaa21d033f523b80c00e767070b7a5651b9
Indigobit <cho_hol@outlook.com>
2015-12-06 21:21:24 +0900

    feat(LogListener): add int value in level enum

--- src/main/java/org/zeropage/log/LogListener.java
+++ src/main/java/org/zeropage/log/LogListener.java
@@ -1,7 +1,17 @@
 package org.zeropage.log;
 
 public interface LogListener {
-    enum Level { FATAL, ERROR, WARN, INFO, DEBUG }
+    enum Level {
+        FATAL(1), ERROR(2), WARN(3), INFO(4), DEBUG(5);
+        private int value;
+
+        Level(int value) {
+            this.value = value;
+        }
+        public int getValue() {
+            return value;
+        }
+    }
 
     void debug(String message);
     void info(String message);
