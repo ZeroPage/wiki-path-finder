7095dadf4b87ff9e7b1c943de243714bd173ba8e
YeongJun Cho <skywave@live.co.kr>
2015-12-07 16:19:26 +0900

    test(WikipediaPathFinder): added testing language and sqlite cache

--- src/test/java/org/zeropage/wiki_path_find/WikipediaPathFinderTest.java
+++ src/test/java/org/zeropage/wiki_path_find/WikipediaPathFinderTest.java
@@ -1,5 +1,6 @@
 package org.zeropage.wiki_path_find;
 
+import org.zeropage.log.LogListener;
 import org.zeropage.wiki_path_find.WikipediaPathFinder;
 import org.zeropage.log.Logger;
 import org.zeropage.log.OutputStreamLogListener;
@@ -8,19 +9,26 @@ import org.junit.Test;
 import org.zeropage.path.RedirectablePath;
 import org.zeropage.wiki_api.WikipediaApi;
 
+import java.io.File;
+
 import static org.junit.Assert.*;
 
 public class WikipediaPathFinderTest {
     private WikipediaPathFinder pathFinder;
+    private WikipediaPathFinder cachePathFinder;
 
     @Before
     public void setUp() throws Exception {
-        pathFinder = new WikipediaPathFinder(new WikipediaApi(WikipediaApi.Language.EN));
+        pathFinder = new WikipediaPathFinder(new WikipediaApi(WikipediaApi.Language.KO));
+        cachePathFinder = new WikipediaPathFinder(new WikipediaApi(WikipediaApi.Language.EN), new File("cache/"));
     }
 
     @Test
     public void testGetPath() throws Exception {
-        RedirectablePath path = pathFinder.getPath("JUnit", "Banana");
-        assertEquals(path.length(), 5);
+        RedirectablePath cachePath = cachePathFinder.getPath("JUnit", "Banana");
+        assertEquals(cachePath.length(), 5);
+
+        RedirectablePath path = pathFinder.getPath("한국", "일본");
+        assertEquals(path.length(), 2);
     }
 }
