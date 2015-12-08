31cd683338f16ffff9c6b0ed9fd19e4fe3845ce8
Indigobit <cho_hol@outlook.com>
2015-12-08 16:30:15 +0900

    docs(LogListener): added JavaDoc

--- src/main/java/org/zeropage/log/LogListener.java
+++ src/main/java/org/zeropage/log/LogListener.java
@@ -1,6 +1,13 @@
 package org.zeropage.log;
 
+
+/**
+ * A class can be listener for Logger when implements LogListener.
+ */
 public interface LogListener {
+    /**
+     * Identifies logging level.
+     */
     enum Level {
         FATAL(1), ERROR(2), WARN(3), INFO(4), DEBUG(5);
         private int value;
@@ -14,13 +21,38 @@ public interface LogListener {
         }
     }
 
+    /**
+     * Logs a message with the debug level.
+     *
+     * @param message an listener to be added
+     */
     void debug(String message);
 
+    /**
+     * Logs a message with the info level.
+     *
+     * @param message an listener to be added
+     */
     void info(String message);
 
+    /**
+     * Logs a message with the warn level.
+     *
+     * @param message an listener to be added
+     */
     void warn(String message);
 
+    /**
+     * Logs a message with the error level.
+     *
+     * @param message an listener to be added
+     */
     void error(String message);
 
+    /**
+     * Logs a message with the fatal level.
+     *
+     * @param message an listener to be added
+     */
     void fatal(String message);
 }
