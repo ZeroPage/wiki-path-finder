939eccfe40eaf4bcaf59d4e448ea6a72e2789d50
kameru <kameru1208@gmail.com>
2015-12-08 16:29:21 +0900

    docs(UnmodifiableIterator): added JavaDoc.

--- src/main/java/org/zeropage/path/UnmodifiableIterator.java
+++ src/main/java/org/zeropage/path/UnmodifiableIterator.java
@@ -2,23 +2,44 @@ package org.zeropage.path;
 
 import java.util.Iterator;
 
+/**
+ * Class implements Iterator
+ * This is Iterator doesn't remove list element.
+ */
 public class UnmodifiableIterator implements Iterator<String> {
+    /**
+     * This is base Iterator, remove() works.
+     */
     Iterator<String> base;
 
+    /**
+     * Class constructor get base iterator.
+     */
     public UnmodifiableIterator(Iterator<String> base) {
         this.base = base;
     }
 
+    /**
+     * same with base iterator.
+     * @return boolean
+     */
     @Override
     public boolean hasNext() {
         return base.hasNext();
     }
 
+    /**
+     * same with base iterator.
+     * @return String
+     */
     @Override
     public String next() {
         return base.next();
     }
 
+    /**
+     * This do nothing because Path must not be removed.
+     */
     @Override
     public void remove() {
     }
