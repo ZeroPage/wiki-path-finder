d0ea0c6695dd70fe7c4df5d65a2c124224401ea8
archetype <archetype93@gmail.com>
2015-12-08 16:39:45 +0900

    docs(CacheLinkSource&MemCacheStorage): added JavaDoc.

--- src/main/java/org/zeropage/cache/CacheLinkSource.java
+++ src/main/java/org/zeropage/cache/CacheLinkSource.java
@@ -5,14 +5,29 @@ import org.zeropage.LinkSourceDecorator;
 
 import java.util.Set;
 
+/**
+ *Implementation of LinkSource Decorator, which retrieves data from cacheStorage.
+ */
 public class CacheLinkSource extends LinkSourceDecorator {
     CacheStorage cache;
 
+    /**
+     *
+     * @param innerObject Object should be wrapped by decorator
+     * @param cacheStorage data source of the class
+     */
     public CacheLinkSource(LinkSource innerObject, CacheStorage cacheStorage) {
         this.innerSource = innerObject;
         this.cache = cacheStorage;
     }
 
+    /**
+     *
+     * retrieving cached data mapped with given keyword parameter
+     * @param from Keyword to be searched from cacheStorage
+     * @return result of search from cacheStorage. If there's no result, pass the task to innerSource.
+     * @throws Exception
+     */
     @Override
     public Set<String> getLinks(String from) throws Exception {
         if (this.cache.hasKey(from)) {
--- src/main/java/org/zeropage/cache/MemCacheStorage.java
+++ src/main/java/org/zeropage/cache/MemCacheStorage.java
@@ -3,23 +3,42 @@ package org.zeropage.cache;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * Implementation of CacheStorage handling cache saved on memory
+ */
 public class MemCacheStorage implements CacheStorage {
     private ConcurrentHashMap<String, Set<String>> cacheTable;
+
 
     public MemCacheStorage() {
         this.cacheTable = new ConcurrentHashMap<>(100, 1);
     }
 
+    /**
+     *
+     * @param key target keyword
+     * @return If cache about keyword exists, return true. Otherwise, return false.
+     */
     @Override
     public boolean hasKey(String key) {
         return cacheTable.containsKey(key);
     }
 
+    /**
+     *
+     * @param key target keyword
+     * @return cached data about keyword. If the data not exists, return null.
+     */
     @Override
     public Set<String> getData(String key) {
         return cacheTable.get(key);
     }
 
+    /**
+     *
+     * @param key target keyword to be saved is key for the map.
+     * @param data Data related with keyword. Saved as value in the map.
+     */
     @Override
     public void setData(String key, Set<String> data) {
         if (key != null && data != null) {
